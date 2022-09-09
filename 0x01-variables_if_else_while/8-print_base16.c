da0467bcd6d19d362d59062c2389939717b6076d
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2022-09-09 12:18:40 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,14 @@ int main(void)
 {
 	char lower;
 
-	for (lower = '0' ; lower <= 'F' ; lower++)
+	for (lower = '0' ; lower <= '9' ; lower++)
 	{
-		if ((lower != ':') && (lower != ';') && (lower != '<') && (lower != '=') && (lower != '>') && (lower != '?') && (lower != '@'))
-		{
-			putchar(lower);
-		}
+		putchar(lower);
 	}
+	for (lower = 'a' ; lower <= 'f' ; lower++)
+        {
+                putchar(lower);
+        }
 	putchar('\n');
 	return (0);
 }
