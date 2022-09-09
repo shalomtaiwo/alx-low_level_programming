4f6a201cf32832ff7cef60659c52d5e81aa7d9e6
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2022-09-08 17:59:16 -0700

    101-error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+	write(2,  err, 59);
+	return (1);
+}
