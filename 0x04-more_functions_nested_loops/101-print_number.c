90deb04b147c0c6e3c8a302ea029fae933d860e3
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2022-09-15 20:35:58 -0700

    task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,29 +8,21 @@
 
 void print_number(int n)
 {
-	unsigned  int m, d, count;
+	unsigned  int n1;
 
 	if (n < 0)
 	{
-		putchar(45);
-		m = n * -1;
+		n1 = -n;
+		_putchar('-');
 	}
 	else
 	{
-		m = n;
+		n1 = n;
 	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
+	
+	if (n1 / 10)
 	{
-		d /= 10;
-		count *= 10;
+		print_number(n1 / 10);
 	}
-
-	for (; count >= 1; count /= 10)
-	{
-		putchar(((m / count) % 10) + 48);
-	}
+	_putchar((n1 % 10) + '0');
 }
